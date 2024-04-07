a2781a3f5a43dd17372273a9d4ee620763af5f41
Thais Guedes <144493551+thafisG@users.noreply.github.com>
2024-04-07 19:38:21 -0300

    Create even_numbers.java

---
+++ control_structure/even_numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println("The number: " + (i) + " is even");
+            }
+        }
+    }
+}
