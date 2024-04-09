5050a4b220de1384099cf7f6acb2edaffad1ba5d
Thais Guedes <144493551+thafisG@users.noreply.github.com>
2024-04-09 20:12:38 -0300

    Create encapsulation.java

---
+++ object_orientation/encapsulation.java
@@ -0,0 +1,12 @@
+public class Car {
+    private String brand; // private attribute
+
+  // get attribute value
+public String getBrand() {
+        return brand;
+    }
+
+  // set attribute value
+public String setBrand(String brand) {
+        this.brand = brand;
+    }
